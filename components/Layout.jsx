42d061a030f2f87468ccbcb97cf607a4725b5ca7
ledidan <didan.mobe@gmail.com>
2022-08-09 23:47:51 +0700

    add viewport mobile

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,6 +8,11 @@ const Layout = ({ children }) => {
       <Head>
         <title>DiDanz Dev Store</title>
         <link rel="icon" href="/images/favicon.ico" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
+        <meta property="og:title" content="Welcome to Dan HeadPhone" key="title" />
       </Head>
       <header>
         <Navbar />
